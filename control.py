e0cc96eb9472ab70092f9f5d292d58d18428b3b1
radhikapriyadarshini <radhikapriyadarshini.eee@gmail.com>
2025-07-27 12:02:50 +0530

    Update control.py

--- control.py
+++ control.py
@@ -1,7 +1,10 @@
-def bess_control(SOC, SOC_min=0.2, SOC_max=0.9):
-    if SOC < SOC_min:
-        return 1.0  # charge
-    elif SOC > SOC_max:
-        return -1.0  # discharge
+def ems_control(P_pv, P_load, SOC, SOC_min=0.2, SOC_max=0.9, max_power=10):
+    """
+    Enhanced EMS control: only allow charge/discharge within SOC limits.
+    """
+    if P_pv > P_load and SOC < SOC_max:
+        return -max_power  # Charging
+    elif P_pv < P_load and SOC > SOC_min:
+        return max_power   # Discharging
     else:
-        return 0.0  # idle
+        return 0.0  # Idle
