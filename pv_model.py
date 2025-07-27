67d5f9ef8327dc00c93116da296a40b775124613
Radhika Priyadarshini <radhikapriyadarshini.eee@gmail.com>
2025-07-27 12:56:31 +0530

    Update pv_model.py

--- pv_model.py
+++ pv_model.py
@@ -1,6 +1,23 @@
-def pv_output(irradiance, temp, Voc=37, Isc=8.21, FF=0.75, G_ref=1000, T_ref=25, alpha=0.005):
-    P = Voc * Isc * FF * (irradiance / G_ref) * (1 - alpha * (temp - T_ref))
-    return P
+import numpy as np
+
+def pv_output(irradiance, temp, Voc_ref=37, Isc_ref=8.21, FF=0.75,
+              G_ref=1000, T_ref=25, beta_Voc=-0.003, alpha_Isc=0.0005):
+    """
+    Estimate PV power output under given irradiance and temperature.
+
+    Returns:
+    - P (float): Estimated PV output power (kW)
+    """
+    irradiance = np.array(irradiance, dtype=float)
+    temp = np.array(temp, dtype=float)
+
+    Voc = Voc_ref * (1 + beta_Voc * (temp - T_ref))
+    Isc = Isc_ref * (irradiance / G_ref) * (1 + alpha_Isc * (temp - T_ref))
+    P = Voc * Isc * FF
+
+    return P / 1000  # kW
 
 if __name__ == "__main__":
-    print("PV Output Test:", pv_output(1000, 25))
+    print("PV Output @ STC:", pv_output(1000, 25))
+    print("PV Output @ Low Irradiance:", pv_output(200, 25))
+    print("PV Output @ High Temp:", pv_output(1000, 45))
